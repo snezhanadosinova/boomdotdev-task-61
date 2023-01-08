a1192e2ed2ed5445f1f2a1a5aeef5bd6bd65ecfc
snezhanadosinova <sdosinova@gmail.com>
2023-01-08 23:21:14 +0200

    feat: added fire emoji to the lowest price

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let elementHotPrice = document.getElementsByClassName("hot");
+  console.log(elementHotPrice);
+
+  let addFireEmoji = elementHotPrice[0].innerText + " 🔥";
+
+  elementHotPrice[0].innerText = addFireEmoji;
 });
